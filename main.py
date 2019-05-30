6a514a8678f697869fe7915b733a4ca478a247e5
Nejc Eržen <nejcerzen14@gmail.com>
2019-05-30 18:13:01 +0200

    Add jinja templating

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    random_text = "Message from the handler!"
+    current_year = datetime.datetime.now().year
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    logged_in = True
+    return render_template("index.html", some_text=random_text, current_year=current_year, cities=cities, logged_in = logged_in)
 
 @app.route("/about")
 def about():
